dd06894c4956d1734586ae1751248afccd2e5d86
JuanMa <juanma.v82@gmail.com>
2013-09-10 17:36:22 -0300

    Test de Parseo

---
+++ test/com/diphot/siu/test/ParseTest.java
@@ -0,0 +1,23 @@
+package com.diphot.siu.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.junit.Test;
+
+public class ParseTest {
+
+	@Test
+	public void parseTest(){
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy",Locale.US).parse("Wed Sep 04 17:10:45 ART 2013");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print( (date.getDay() +1 )+ "/" + (date.getMonth() +1 ) + "/"+ (date.getYear()+ 1900));
+	}
+}
