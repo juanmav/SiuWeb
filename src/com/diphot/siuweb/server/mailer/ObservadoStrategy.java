541b8a58e4596c5716184f63594603a2ec15f70b
JuanMa <juanma.v82@gmail.com>
2013-10-02 14:52:29 -0300

    [#83] Enviar Mail en Inspección OBSERVADA

---
+++ src/com/diphot/siuweb/server/mailer/ObservadoStrategy.java
@@ -0,0 +1,42 @@
+package com.diphot.siuweb.server.mailer;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.List;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+
+import com.diphot.siuweb.server.business.model.Inspeccion;
+import com.diphot.siuweb.server.business.model.Role;
+import com.diphot.siuweb.server.business.model.User;
+import com.diphot.siuweb.server.pesistense.daos.RoleDAO;
+import com.diphot.siuweb.server.pesistense.daos.UserDAO;
+import com.diphot.siuweb.shared.SiuConstants;
+
+
+public class ObservadoStrategy extends InspeccionMailStrategy{
+	@Override
+	public void exec(Inspeccion inspeccion, Message msg) {
+		// Tengo que buscar a los usuarios que son supervisores.
+		RoleDAO rdao = new RoleDAO();
+		rdao.begin();
+		Role supr = rdao.getRoleByName(SiuConstants.ROLES.SUPERVISOR);
+		// agrego a los Inspectores
+		HashSet<User> users = supr.getMembers();
+		try {
+			for (User u : users){
+				System.out.println(u.getUsername());
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(u.getEmail(), u.getUsername()));
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		rdao.end();
+	}
+}
