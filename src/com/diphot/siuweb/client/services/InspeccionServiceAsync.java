d3c9d3033ebda453e9e343f50484950ee86283c8
juanmav <juanma.v82@gmail.com>
2013-11-23 16:37:24 -0300

    [#157] Primera version mapa inspecciones. Hay que mejorarlo.

--- src/com/diphot/siuweb/client/services/InspeccionServiceAsync.java
+++ src/com/diphot/siuweb/client/services/InspeccionServiceAsync.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 import com.diphot.siuweb.shared.dtos.EncodedImageDTO;
 import com.diphot.siuweb.shared.dtos.InspeccionDTO;
+import com.diphot.siuweb.shared.dtos.filters.InspeccionFilterDTO;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface InspeccionServiceAsync {
 	void create(InspeccionDTO dto, AsyncCallback<Long> callback);
 	void getList(AsyncCallback<ArrayList<InspeccionDTO>> callback);
-	void createImage(EncodedImageDTO dto, int numero,
-			AsyncCallback<Void> callback);
+	void createImage(EncodedImageDTO dto, int numero, AsyncCallback<Void> callback);
 	void examplesCreate(AsyncCallback<Void> callback);
+	void getInspeccionesDTO(InspeccionFilterDTO filter, AsyncCallback<ArrayList<InspeccionDTO>> callback);
 }
